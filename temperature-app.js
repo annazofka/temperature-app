56c78e7c734d285473cfe5918ca825fddf804638
Kocia <annazofka@gmail.com>
2023-08-26 23:18:29 +0100

    format temperature

--- temperature-app.js
+++ temperature-app.js
@@ -3,5 +3,5 @@ let result = document.querySelector('#result')
 
 temp.addEventListener('input', () => {
 	let celcius = ((parseFloat(temp.value) - 32) * 5) / 9
-	result.textContent = celcius.toFixed(2)
+	result.textContent = `${celcius.toFixed(1)} degrees Celcius`
 })
