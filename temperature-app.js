14529296e7ead31489ba038e306440d901e93924
Kocia <annazofka@gmail.com>
2023-08-27 03:57:11 +0100

    add toggle for temperature units

--- temperature-app.js
+++ temperature-app.js
@@ -1,7 +1,25 @@
 let temp = document.querySelector('#temp')
 let result = document.querySelector('#result')
+let toggles = document.querySelectorAll('[name="units"]')
 
 temp.addEventListener('input', () => {
-	let celcius = ((parseFloat(temp.value) - 32) * 5) / 9
-	result.textContent = `${celcius.toFixed(1)} degrees Celcius`
+	let units = 'celsius'
+	for (let toggle of toggles) {
+		if (toggle.checked) {
+			units = toggle.value
+		}
+	}
+
+	let converted
+	if (units === 'celsius') {
+		converted = ((parseFloat(temp.value) - 32) * 5) / 9
+	} else {
+		converted = (parseFloat(temp.value) * 9) / 5 + 32
+	}
+
+	if (temp.value === '') {
+		converted = 0
+	}
+
+	result.textContent = `${converted.toFixed(1)} degrees ${units}`
 })
